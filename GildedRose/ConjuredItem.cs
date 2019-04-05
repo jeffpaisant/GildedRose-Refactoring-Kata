1d4a058fa0e108667ca1eacc576c019505ea4c75
jfpaisant <jean-francois.paisant@outlook.com>
2019-04-05 13:49:49 +0200

    feat: Add conjured item

---
+++ GildedRose/ConjuredItem.cs
@@ -0,0 +1,43 @@
+﻿namespace GildedRose
+{
+    public class ConjuredItem : Item
+    {
+
+        public void DecrementSellIn()
+        {
+            this.SellIn--;
+        }
+
+        public void DecrementQuality()
+        {
+            this.Quality -= 2;
+        }
+
+        public bool IsQualityGreaterThan0()
+        {
+            return this.Quality >= 0;
+        }
+
+        public void ManageQuality()
+        {
+            if (this.CanDecreaseQuality())
+            {
+                this.DecrementQuality();
+            }
+            else
+            {
+                this.ResetQuality();
+            }
+        }
+
+        private void ResetQuality()
+        {
+            this.Quality = 0;
+        }
+
+        private bool CanDecreaseQuality()
+        {
+            return this.Quality - 2 > 0;
+        }
+    }
+}
